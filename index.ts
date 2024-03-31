9e3d12dfd4145a382e864fee87d46765be8dbed2
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2024-03-31 02:36:31 -0500

    removed unneeded log

--- index.ts
+++ index.ts
@@ -22,8 +22,6 @@ import along from '@turf/along';
 const snowPiercerShape = JSON.parse(fs.readFileSync('./snowPiercer.json', 'utf8'));
 const snowPiercerShapeLength = length(snowPiercerShape);
 
-console.log(snowPiercerShapeLength)
-
 const calculateSnowPiercerPosition = (time: Date) => {
   const timesAround = Math.abs(Number(((time.valueOf() - new Date('2024-04-01T00:00:00-05:00').valueOf()) / (1000 * 60 * 60 * 6)% 1).toFixed(4)));
   const distanceOnShape = snowPiercerShapeLength * timesAround;
